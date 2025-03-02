2368e4fb9b48899d59a78f8c7ffe704875d56cc8
samah40 <samahalielkady22@gmail.com>
2025-03-02 13:48:38 +0200

    Adding files

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,4 @@ export default defineConfig({
 
 
 
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/samah40/fresh-cart.git
-git push -u origin main
 
